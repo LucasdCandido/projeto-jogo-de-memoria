f6ae461c8693e045aab757b9ede0e789815ec35d
Lucas Dourado Candido <lucasdcandido1994@gmail.com>
2022-08-07 22:27:40 -0300

    projeto finalizado

--- js/game.js
+++ js/game.js
@@ -1,4 +1,6 @@
 const grid = document.querySelector('.grid');
+const spanPlayer = document.querySelector('.player');
+const timer = document.querySelector('.timer')
 
 const character = [
   'beth',
@@ -26,7 +28,8 @@ const checkEndGame = () => {
   const disabledCards = document.querySelectorAll('.disabled-card');
 
   if (disabledCards.length === 20) {
-    alert('Parabéns, Você conseguiu');
+    clearInterval(this.loop);
+    alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}.`)
   }
 }
 
@@ -106,4 +109,23 @@ const loadGame = () => {
   });
 }
 
-loadGame();
+
+const statTimer = () => {
+  this.loop = setInterval(()=>{
+
+    const currentTime = +timer.innerHTML;
+    timer.innerHTML = currentTime + 1;
+
+  }, 1000);
+}
+
+window.onload = () => {
+
+  spanPlayer.innerHTML = localStorage.getItem('player');
+  statTimer();
+  loadGame();
+
+
+}
+
+
